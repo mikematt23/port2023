5fd1fa89ac9f6d776c4f2421a9c51f83d44c8a45
Michael MAtteis <michaelcmatteis@gmail.com>
2023-01-30 00:15:38 -0600

    making the site more user friendly

--- home.js
+++ home.js
@@ -1,22 +1,26 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close') 
 const project1 = document.getElementById('project1')
-// const project2 = document.getElementById('project2')
 const project3 = document.getElementById('project3')
 const project4 = document.getElementById('project4')
+const projectHolder = document.getElementById('projectHolder')
 
 open.addEventListener('click',()=>{
   console.log('here')
    project1.className = 'project1'
-  //  project2.className = 'project2'
    project3.className = 'project3'
    project4.className = 'project4'
+   projectHolder.style.display = 'block'
 })
+
+function closeProjectHolder(){
+  projectHolder.style.display = "none"
+}
 
 close.addEventListener('click',()=>{
   console.log('clicked')
   project1.className = 'closeProject1'
-  // project2.className = "closeProject2"
   project3.className = 'closeProject3'
   project4.className = "closeProject4"
 })
+
